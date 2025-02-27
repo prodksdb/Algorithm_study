5f0c40de747960d61f67295afded6a8a1b3ba1b6
prodksdb <150729023+prodksdb@users.noreply.github.com>
2025-02-27 13:30:51 +0900

    [D1] Title: N줄덧셈, Time: 151 ms, Memory: 20,740 KB -BaekjoonHub

---
+++ SWEA/D1/2025. N줄덧셈/N줄덧셈.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		int sum = 0;
+		for(int i = 1; i <= N; i++) {
+			sum += i;
+		}
+		
+		bw.write(String.valueOf(sum));
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
